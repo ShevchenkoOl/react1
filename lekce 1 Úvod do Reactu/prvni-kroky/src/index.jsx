b95570110ce26bc52f5fc3c4b0839fec931e5b2f
ShevchenkoOL <uzlabini@gmail.com>
2024-09-12 21:07:32 +0200

    modified lekce 1

--- lekce 1 Úvod do Reactu/prvni-kroky/src/index.jsx
+++ lekce 1 Úvod do Reactu/prvni-kroky/src/index.jsx
@@ -3,7 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./style.css";
 
 // Uložte si JSX představující obsah stránky do seperátní proměnné App uvnitř index.jsx a tu pak použijte ve funkci render.
-
+// 1. Když je tělo funkce napsáno ve složených závorkách {}, musíte vrátit výsledek pomocí return.
+// 2. Při použití zkrácené syntaxe závorek () se JSX vrátí implicitně (неявно) a return není vyžadován.
+// Pokud je ve funkci logika, jako jsou deklarace proměnných nebo jiné operace, bez return nebude JSX vráceno a komponenta nic nevykreslí.
 const App = () => {
   const name = "Oleksii Shvchenko";
   /*Uložte si své jméno do proměnné a obsah této proměnné vložte do hlavičky stránky.*/
@@ -12,7 +14,7 @@ const App = () => {
   return (
     <div className="container">
       <header>
-        <h1>{name}</h1>{" "}
+        <h1>{name}</h1>
         {/* 4. Přímo ve volání funkce render změňte nadpis stránky h1 a v hlavičce uveďte svoje jméno */}
       </header>
       <main>
